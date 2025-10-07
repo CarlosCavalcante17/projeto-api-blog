baea10f713dcfd5d861bf717ce2eb24022285aa4
Carlos <caducavalcante2013@gmail.com>
2025-10-06 22:06:58 -0300

    modifiquei o index

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
-import Express from "express";
+import express from "express";
 import 'dotenv/config';
 import { setupSwagger } from './swagger';
+import UsuarioRoutes from './Routes/UsuarioRoutes'
+import PostRoutes from './Routes/PostRoutes'
+import ComentarioRoutes from './Routes/ComentarioRoutes'
 
-const app = Express();
-app.use(Express.json());
+const app = express();
+app.use(express.json());
 setupSwagger(app);
-app.use('', );
+app.use('/Usuarios', UsuarioRoutes );
+app.use('/Post', PostRoutes);
+app.use('/Comentario', ComentarioRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`O servidor está rodando no http://localhost:${PORT}`);
+    console.log(`Swagger docs disponíveis em http://localhost:${PORT}/api-docs`);
+
 });
 
 
